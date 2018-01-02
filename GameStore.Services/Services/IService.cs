ec2583292a873fddc8162563029285f9a87aa784
Aleks B <aleksandroboko@gmail.com>
2018-01-02 16:49:31 +0200

    Add autofac. Add common interface for services

---
+++ GameStore.Services/Services/IService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Services.Services
+{
+    interface IService<T>
+    {
+        ICollection<T> GetAll();
+        T GetItemById(Guid id);
+        void Add(T item);
+        void Update(T item);
+        void Remove(T item);
+    }
+}
