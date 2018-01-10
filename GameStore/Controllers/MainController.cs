ba26b9ce28d0b309048fb473f5325c432f4ed61a
Aleks B <aleksandroboko@gmail.com>
2018-01-10 11:53:06 +0200

    Add WebApi Controller - Main and Index page

---
+++ GameStore/Controllers/MainController.cs
@@ -0,0 +1,27 @@
+﻿using GameStore.Domains.Domain;
+using GameStore.Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GameStore.Controllers
+{
+    public class MainController : ApiController
+    {
+        public MainController(IService<GameModel> gameService)
+        {
+            this.gameService = gameService;
+        }
+
+        private readonly IService<GameModel> gameService;
+
+        public IEnumerable<GameModel> Get()
+        {
+            var games = gameService.GetAll();
+            return games;
+        }
+    }
+}
