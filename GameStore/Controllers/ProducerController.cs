e636d3d67efc4253945ee384da1a808bdbf58e50
Aleks B <aleksandroboko@gmail.com>
2018-01-15 15:33:22 +0200

    Add producers to game form

---
+++ GameStore/Controllers/ProducerController.cs
@@ -0,0 +1,24 @@
+﻿using GameStore.Domains.Domain;
+using GameStore.Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GameStore.Controllers
+{
+    [RoutePrefix("api/producer")]
+    public class ProducerController : ApiController
+    {
+        public ProducerController(IProducerService producerService)
+        {
+            this.producerService = producerService;
+        }
+
+        private readonly IProducerService producerService;
+
+        public IEnumerable<ProducerInfoTransferModel> GetProducers() => producerService.GetProducerInfoTransferAll();
+    }
+}
